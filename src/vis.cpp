aa922ee10e865664e8b5373e7aabac9b602255bc
Jonathan Zrake <jonathan.zrake@gmail.com>
2014-01-18 18:48:55 -0800

    Write a working draft of a GUI visualization module

--- src/vis.cpp
+++ src/vis.cpp
@@ -3,218 +3,101 @@ extern "C" {
 }
 #if (M2_HAVE_GLUT)
 
-/****************************************************************************
 
-  example3.cpp
-
-  A GLUT program using all the features of the GLUI User Interface Library
-  (except columns, featured in example4.cpp)
-
-  -----------------------------------------------------------------------
-	   
-  9/9/98 Paul Rademacher (rademach@cs.unc.edu)
-
-****************************************************************************/
 
 #include <string>
 #include <map>
 #include <cmath>
 #include "glui/glui.h"
 
-int   last_x, last_y;
-float xy_aspect;
-float rotationX = 0.0, rotationY = 0.0;
-
-
-/** These are the live variables passed into GLUI ***/
-int   obj_type = 0;
-int   ColorTable = 3;
-int   main_window;
-int   counter = 0;
-int   AutoPlay = 0;
-int   LogScaling = 0;
-int   DrawMesh = 0;
-float scale = 0.0;
-std::string text = "";
-
-
-/********** User IDs for callbacks ********/
-#define OPEN_CONSOLE_ID      100
-#define CMD_HIST_RESET_ID    101
-#define CMD_CLOSE_ID         102
-#define LIGHT0_ENABLED_ID    200
-#define LIGHT1_ENABLED_ID    201
-#define LIGHT0_INTENSITY_ID  250
-#define LIGHT1_INTENSITY_ID  251
-#define CHANGE_COLORTABLE_ID 350
-#define CHANGE_FIELD_ID      351
-#define CHANGE_LOGSCALING_ID 352
-#define CHANGE_DRAWMESH_ID   353
-
-
 
 static GLUI *glui;
-class M2GL_Dataset;
-static m2sim *M2;
-static std::vector<int> obj_keys;
-static void color_map(double val, GLfloat rgb[3], int ct);
-static M2GL_Dataset *dataset;
+static int last_x, last_y;
+static int main_window;
+static float xy_aspect;
 
 
-class M2GL_Dataset
+
+
+class SimulationController
 {
 private:
-  m2sim *m2;
-  int DataMember;
-  //  int ColorTable;
-  //  int DrawMesh;
-  //  int LogScaling;
-  double DataRange[2];
-  GLfloat *VertexData;
-  GLfloat *ColorData;
+  int user_id;
+  enum {
+    ACTION_RESET_VIEW,
+    ACTION_STEP,
+    ACTION_QUIT,
+  } ;
 public:
-  M2GL_Dataset(m2sim *m2) :
-    m2(m2),
-    DataMember(M2_COMOVING_MASS_DENSITY),
-    //    ColorTable(3),
-    //    DrawMesh(0),
-    //    LogScaling(0),
-    VertexData(NULL),
-    ColorData(NULL)
-  { }
-  void reload_data()
-  {
-    int n;
-    int *L = m2->local_grid_size;
-    m2vol *V;
-    double y;
-    double x00[4]; /* vertex coordinates */
-    double x01[4];
-    double x11[4];
-    double x10[4];
-    double x00c[4]; /* in cartesian geometry */
-    double x01c[4];
-    double x11c[4];
-    double x10c[4];
+  float rotationX;
+  float rotationY;
 
+  /* live variables */
+  float ZoomLevel;
+  int AutoAdvance;
 
-    VertexData = (GLfloat*) realloc(VertexData, L[0]*3*4*sizeof(GLfloat));
-    ColorData = (GLfloat*) realloc(ColorData, L[0]*3*sizeof(GLfloat));
-    DataMember = M2_COMOVING_MASS_DENSITY;//obj_keys[obj_type];
-
-
-    for (n=0; n<L[0]; ++n) {
-      V = m2->volumes + n;
-
-      y = m2aux_get(&V->aux, DataMember);
-
-      if (LogScaling) {
-	if (y < 1e-6) y = 1e-6;
-	y = log10(fabs(y));
-      }
-
-      if (n == 0) {
-	DataRange[0] = y;
-	DataRange[1] = y;
-      }
-      else {
-	if (y < DataRange[0]) DataRange[0] = y;
-	if (y > DataRange[1]) DataRange[1] = y;
-      }
-    }
-
-    for (n=0; n<L[0]; ++n) {
-      V = m2->volumes + n;
-
-      x00[1] = V->x0[1];
-      x00[2] = V->x0[2];
-      x00[3] = V->x0[3];
-      x01[1] = V->x1[1];
-      x01[2] = V->x0[2];
-      x01[3] = V->x0[3];
-      x11[1] = V->x1[1];
-      x11[2] = V->x1[2];
-      x11[3] = V->x0[3];
-      x10[1] = V->x0[1];
-      x10[2] = V->x1[2];
-      x10[3] = V->x0[3];
-
-      m2_to_cartesian(x00, x00c, m2->geometry);
-      m2_to_cartesian(x01, x01c, m2->geometry);
-      m2_to_cartesian(x11, x11c, m2->geometry);
-      m2_to_cartesian(x10, x10c, m2->geometry);
-
-      VertexData[4*3*n + 0*3 + 0] = x00c[1];
-      VertexData[4*3*n + 0*3 + 1] = x00c[2];
-      VertexData[4*3*n + 0*3 + 2] = x00c[3];
-      VertexData[4*3*n + 1*3 + 0] = x01c[1];
-      VertexData[4*3*n + 1*3 + 1] = x01c[2];
-      VertexData[4*3*n + 1*3 + 2] = x01c[3];
-      VertexData[4*3*n + 2*3 + 0] = x11c[1];
-      VertexData[4*3*n + 2*3 + 1] = x11c[2];
-      VertexData[4*3*n + 2*3 + 2] = x11c[3];
-      VertexData[4*3*n + 3*3 + 0] = x10c[1];
-      VertexData[4*3*n + 3*3 + 1] = x10c[2];
-      VertexData[4*3*n + 3*3 + 2] = x10c[3];
-
-      y = m2aux_get(&V->aux, DataMember);
-
-      if (LogScaling) {
-	y = log10(fabs(y));
-      }
-
-      y -= DataRange[0];
-      y /= DataRange[1] - DataRange[0];
-      color_map(y, &ColorData[3*n], ColorTable);
-    }
-  }
-  void draw()
-  {
-    glDisable(GL_LIGHTING);
-    for (int n=0; n<m2->local_grid_size[0]; ++n) {
-      if (DrawMesh) {
-	glBegin(GL_LINE_LOOP);
-      }
-      else {
-	glBegin(GL_QUADS);
-      }
-      glColor3fv(&ColorData[3*n]);
-      glVertex3fv(&VertexData[4*3*n + 0*3]);
-      glVertex3fv(&VertexData[4*3*n + 1*3]);
-      glVertex3fv(&VertexData[4*3*n + 2*3]);
-      glVertex3fv(&VertexData[4*3*n + 3*3]);
-
-      glEnd();
-    }
-    glEnable(GL_LIGHTING);
-  }
-  void keyboard(int key, int x, int y)
-  {
-    switch (key) {
-    case 'f':
-      ++obj_type %= obj_keys.size();
-      this->reload_data();
-      GLUI_Master.sync_live_all();
-      break;
-    case 'm':
-      DrawMesh ^= 1;
-      GLUI_Master.sync_live_all();
-      break;
-    case 'l':
-      LogScaling ^= 1;
-      this->reload_data();
-      GLUI_Master.sync_live_all();
-      break;
-    }
-  }
+  SimulationController(GLUI_Panel *parent);
+  void keyboard(int key, int x, int y) { }
+  void draw();
+  void refresh();
+  static std::map<int, SimulationController*> instances;
+  static void refresh_cb(int user_id);
+  static void action_cb(int action_id);
 } ;
+std::map<int, SimulationController*> SimulationController::instances;
+
+
+class DatasetController
+{
+private:
+  double DataRange[2];
+  GLfloat *ColorData;
+  GLfloat *VertexData;
+  double AngularOffset;
+  int user_id;
+  std::vector<int> obj_keys;
+
+  GLUI_StaticText *text_min;
+  GLUI_StaticText *text_max;
+
+  /* live variables */
+  int DrawMesh;
+  int LogScale;
+  int ColorMap;
+  int DataMember;
+public:
+  DatasetController(GLUI_Panel *parent);
+  void SetAngularOffset(double x) { AngularOffset = x; }
+  void keyboard(int key, int x, int y) { }
+  void draw();
+  void refresh();
+  static std::map<int, DatasetController*> instances;
+  static void refresh_cb(int user_id);
+} ;
+std::map<int, DatasetController*> DatasetController::instances;
+
+
+
+static SimulationController *sim_controller = NULL;
+static m2sim *M2;
+static void color_map(double val, GLfloat rgb[3], int ct);
+
+
+
+
+
+
+
+
+
+
 void myGlutKeyboard(unsigned char Key, int x, int y)
 {
   if (Key == 27) {
     exit(0);
   }
   else {
-    dataset->keyboard(Key, x, y);
+    //    dataset->keyboard(Key, x, y);
   }
   glutPostRedisplay();
 }
@@ -242,27 +125,26 @@ void myGlutIdle(void)
   /*   the live ones all at once with a single call to sync_live  */
   /****************************************************************/
 
-  counter++;
   glui->sync_live();
 
-  if (AutoPlay) {
+  if (sim_controller->AutoAdvance) {
     m2sim_drive(M2);
-    dataset->reload_data();
+    sim_controller->refresh();
   }
 }
 
 
 void myGlutMouse(int button, int button_state, int x, int y)
 {
-  if ( button == GLUT_LEFT_BUTTON && button_state == GLUT_DOWN ) {
+  if (button == GLUT_LEFT_BUTTON && button_state == GLUT_DOWN) {
     last_x = x;
     last_y = y;
   }
 }
 void myGlutMotion(int x, int y)
 {
-  rotationX += (float) (y - last_y);
-  rotationY += (float) (x - last_x);
+  sim_controller->rotationX += (float) (y - last_y);
+  sim_controller->rotationY += (float) (x - last_x);
   last_x = x;
   last_y = y;
   glutPostRedisplay(); 
@@ -273,105 +155,10 @@ void myGlutReshape(int x, int y)
   glViewport(0, 0, x, y);
   glutPostRedisplay();
 }
-void myGlutDisplay( void )
+void myGlutDisplay(void)
 {
-  GLfloat zoom = powf(10.0, scale);
-  glClearColor(.9f, .9f, .9f, 1.0f);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glFrustum(-xy_aspect*0.08, xy_aspect*0.08, -0.08, 0.08, 0.1, 15.0);
-
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-  glTranslatef(0.0, 0.0, -1.0f);
-  glRotatef(rotationX, 1.0, 0.0, 0.0);
-  glRotatef(rotationY, 0.0, 1.0, 0.0);
-  glRotated(-90.0, 1.0, 0.0, 0.0);
-  glScalef(zoom, zoom, zoom);
- 
-
-  dataset->draw();
-
-
-  /* Disable lighting and set up ortho projection to render text */
-  glDisable(GL_LIGHTING);  
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluOrtho2D(0.0, 100.0, 0.0, 100.0);
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-  glColor3ub(0, 0, 0);
-  glRasterPos2i(10, 10);
-  for (unsigned int i=0; i<text.length(); ++i) {
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text[i]);
-  }
-  glEnable(GL_LIGHTING);
-
+  sim_controller->draw();
   glutSwapBuffers(); 
-}
-
-
-
-class MyClass
-{
-private:
-  int user_id;
-  int val1, val2, val3, val4;
-public:
-  MyClass(GLUI_Panel *parent);
-  void keyboard(int key, int x, int y) { }
-  void draw() { }
-  void refresh() { }
-  static std::map<int, MyClass*> instances;
-  static void refresh_cb(int user_id)
-  {
-    instances[user_id]->refresh();
-  }
-} ;
-std::map<int, MyClass*> MyClass::instances;
-MyClass::MyClass(GLUI_Panel *parent)
-{
-  user_id = instances.size();
-  instances[user_id] = this;
-  new GLUI_Column(parent);
-  new GLUI_Checkbox(parent, "check1", &val1, user_id, refresh_cb);
-  new GLUI_Checkbox(parent, "check2", &val2, user_id, refresh_cb);
-  new GLUI_Checkbox(parent, "check3", &val3, user_id, refresh_cb);
-  new GLUI_Checkbox(parent, "check4", &val4, user_id, refresh_cb);
-}
-
-
-
-
-class SimulationController
-{
-private:
-  int user_id;
-  int val1, val2, val3, val4;
-public:
-  SimulationController(GLUI_Panel *parent);
-  void keyboard(int key, int x, int y) { }
-  void draw() { }
-  void refresh() { }
-  static std::map<int, SimulationController*> instances;
-  static void refresh_cb(int user_id)
-  {
-    instances[user_id]->refresh();
-  }
-} ;
-std::map<int, SimulationController*> SimulationController::instances;
-SimulationController::SimulationController(GLUI_Panel *parent)
-{
-  user_id = instances.size();
-  instances[user_id] = this;
-  new GLUI_Column(parent);
-  new GLUI_Checkbox(parent, "check1", &val1, user_id, refresh_cb);
-  new GLUI_Checkbox(parent, "check2", &val2, user_id, refresh_cb);
-  new GLUI_Checkbox(parent, "check3", &val3, user_id, refresh_cb);
-  new GLUI_Checkbox(parent, "check4", &val4, user_id, refresh_cb);
-  new GLUI_Button(parent, "Quit", 0, exit);
 }
 
 
@@ -399,28 +186,256 @@ void m2sim_visualize(m2sim *m2, int argc, char **argv)
   glEnable(GL_DEPTH_TEST);
 
 
+  /* Build the GUI */
   glui = GLUI_Master.create_glui("GLUI", 0, 768+50, 50);
   new GLUI_StaticText(glui, "M2 controls"); 
-
-
   GLUI_Panel *dataset_panel = new GLUI_Panel(glui, "data sets");
-  new MyClass(dataset_panel);
-  new MyClass(dataset_panel);
-
-
+  DatasetController *ds1 = new DatasetController(dataset_panel);
+  new GLUI_Column(dataset_panel);
+  DatasetController *ds2 = new DatasetController(dataset_panel);
   GLUI_Panel *general_panel = new GLUI_Panel(glui, "simulation controls");
-  new SimulationController(general_panel);
+  sim_controller = new SimulationController(general_panel);
 
+  ds1->SetAngularOffset(M2_PI);
+  ds2->SetAngularOffset(0.0);
 
-  dataset = new M2GL_Dataset(m2);
-  dataset->reload_data();
-
-
+  sim_controller->refresh();
 
   glui->set_main_gfx_window(main_window);
   GLUI_Master.set_glutIdleFunc(myGlutIdle);
   glutMainLoop();
 }
+
+
+
+
+SimulationController::SimulationController(GLUI_Panel *parent)
+  : ZoomLevel(0.0),
+    AutoAdvance(0)
+{
+  user_id = instances.size();
+  instances[user_id] = this;
+  new GLUI_Checkbox(parent, "Auto advance", &AutoAdvance);
+  GLUI_Scrollbar *zoom_sb = new GLUI_Scrollbar(parent,
+					       "zoom level",
+					       GLUI_SCROLL_HORIZONTAL,
+					       &ZoomLevel);
+  new GLUI_Button(parent, "Reset view", ACTION_RESET_VIEW, action_cb);
+  new GLUI_Button(parent, "Step", ACTION_STEP, action_cb);
+  new GLUI_Button(parent, "Quit", ACTION_QUIT, action_cb);
+  zoom_sb->set_float_limits(-2.0, 2.0);
+}
+void SimulationController::refresh_cb(int user_id)
+{
+  instances[user_id]->refresh();
+}
+void SimulationController::action_cb(int action_id)
+{
+  switch (action_id) {
+  case ACTION_RESET_VIEW:
+    sim_controller->ZoomLevel = 0.0;
+    sim_controller->rotationX = 0.0;
+    sim_controller->rotationY = 0.0;
+    break;
+  case ACTION_STEP:
+    m2sim_drive(M2);
+    sim_controller->refresh();
+    break;
+  case ACTION_QUIT:
+    exit(0);
+    break;
+  }
+}
+void SimulationController::refresh()
+{
+  std::map<int, DatasetController*>::iterator d =
+    DatasetController::instances.begin();
+  while (d != DatasetController::instances.end()) {
+    d->second->refresh();
+    ++d;
+  }
+}
+void SimulationController::draw()
+{
+  GLfloat zoom = powf(10.0, ZoomLevel);
+
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glFrustum(-xy_aspect*0.08, xy_aspect*0.08, -0.08, 0.08, 0.1, 15.0);
+
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  glTranslatef(0.0, 0.0, -1.0f);
+  glRotatef(rotationX, 1.0, 0.0, 0.0);
+  glRotatef(rotationY, 0.0, 1.0, 0.0);
+  glRotated(-90.0, 1.0, 0.0, 0.0);
+  glScalef(zoom, zoom, zoom);
+
+
+  std::map<int, DatasetController*>::iterator d =
+    DatasetController::instances.begin();
+  while (d != DatasetController::instances.end()) {
+    d->second->draw();
+    ++d;
+  }
+
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(0.0, 100.0, 0.0, 100.0);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  glColor3ub(0, 0, 0);
+  glRasterPos2i(10, 10);
+
+  std::string text = "test";
+
+  for (unsigned int i=0; i<text.length(); ++i) {
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text[i]);
+  }
+}
+
+
+DatasetController::DatasetController(GLUI_Panel *parent)
+  :
+  ColorData(NULL),
+  VertexData(NULL),
+  AngularOffset(0.0),
+
+  /* live variables */
+  DrawMesh(0),
+  LogScale(0),
+  ColorMap(3),
+  DataMember(0)
+{
+  user_id = instances.size();
+  instances[user_id] = this;
+  GLUI_RadioGroup *radio = new GLUI_RadioGroup(parent, &DataMember, user_id, refresh_cb);
+  new GLUI_RadioButton(radio, "mass density"); obj_keys.push_back(M2_COMOVING_MASS_DENSITY);
+  new GLUI_RadioButton(radio, "gas pressure"); obj_keys.push_back(M2_GAS_PRESSURE);
+  new GLUI_RadioButton(radio, "v1"); obj_keys.push_back(M2_VELOCITY1);
+  new GLUI_RadioButton(radio, "v2"); obj_keys.push_back(M2_VELOCITY2);
+  new GLUI_RadioButton(radio, "v3"); obj_keys.push_back(M2_VELOCITY3);
+  new GLUI_RadioButton(radio, "B1"); obj_keys.push_back(M2_MAGNETIC1);
+  new GLUI_RadioButton(radio, "B2"); obj_keys.push_back(M2_MAGNETIC2);
+  new GLUI_RadioButton(radio, "B3"); obj_keys.push_back(M2_MAGNETIC3);
+  new GLUI_Separator(parent);
+  text_min = new GLUI_StaticText(parent, "min");
+  text_max = new GLUI_StaticText(parent, "max");
+  new GLUI_Separator(parent);
+  new GLUI_Checkbox(parent, "draw mesh", &DrawMesh, user_id, refresh_cb);
+  new GLUI_Checkbox(parent, "log scale", &LogScale, user_id, refresh_cb);
+  GLUI_Spinner *cm = new GLUI_Spinner (parent, "color map", &ColorMap, user_id, refresh_cb);
+  cm->set_int_limits(0, 5);
+  this->refresh();
+}
+void DatasetController::draw()
+{
+  for (int n=0; n<M2->local_grid_size[0]; ++n) {
+    if (DrawMesh) {
+      glBegin(GL_LINE_LOOP);
+    }
+    else {
+      glBegin(GL_QUADS);
+    }
+    glColor3fv(&ColorData[3*n]);
+    glVertex3fv(&VertexData[4*3*n + 0*3]);
+    glVertex3fv(&VertexData[4*3*n + 1*3]);
+    glVertex3fv(&VertexData[4*3*n + 2*3]);
+    glVertex3fv(&VertexData[4*3*n + 3*3]);
+    glEnd();
+  }
+}
+void DatasetController::refresh()
+{
+  int *L = M2->local_grid_size;
+  double y;
+  double x00[4]; /* vertex coordinates */
+  double x01[4];
+  double x11[4];
+  double x10[4];
+  double x00c[4]; /* in cartesian geometry */
+  double x01c[4];
+  double x11c[4];
+  double x10c[4];
+  m2vol *V;
+  VertexData = (GLfloat*) realloc(VertexData, L[0]*3*4*sizeof(GLfloat));
+  ColorData = (GLfloat*) realloc(ColorData, L[0]*3*sizeof(GLfloat));
+  for (int n=0; n<L[0]; ++n) {
+    V = M2->volumes + n;
+    y = m2aux_get(&V->aux, obj_keys[DataMember]);
+    if (LogScale) {
+      if (y < 1e-6) y = 1e-6;
+      y = log10(fabs(y));
+    }
+    if (n == 0) {
+      DataRange[0] = y;
+      DataRange[1] = y;
+    }
+    else {
+      if (y < DataRange[0]) DataRange[0] = y;
+      if (y > DataRange[1]) DataRange[1] = y;
+    }
+  }
+
+  char min[256];
+  char max[256];
+  snprintf(min, 256, "min: %3.2f", DataRange[0]);
+  snprintf(max, 256, "max: %3.2f", DataRange[1]);
+  text_min->set_text(min);
+  text_max->set_text(max);
+
+  for (int n=0; n<L[0]; ++n) {
+    V = M2->volumes + n;
+    x00[1] = V->x0[1];
+    x00[2] = V->x0[2];
+    x00[3] = V->x0[3] + AngularOffset;
+    x01[1] = V->x1[1];
+    x01[2] = V->x0[2];
+    x01[3] = V->x0[3] + AngularOffset;
+    x11[1] = V->x1[1];
+    x11[2] = V->x1[2];
+    x11[3] = V->x0[3] + AngularOffset;
+    x10[1] = V->x0[1];
+    x10[2] = V->x1[2];
+    x10[3] = V->x0[3] + AngularOffset;
+    m2_to_cartesian(x00, x00c, M2->geometry);
+    m2_to_cartesian(x01, x01c, M2->geometry);
+    m2_to_cartesian(x11, x11c, M2->geometry);
+    m2_to_cartesian(x10, x10c, M2->geometry);
+    VertexData[4*3*n + 0*3 + 0] = x00c[1];
+    VertexData[4*3*n + 0*3 + 1] = x00c[2];
+    VertexData[4*3*n + 0*3 + 2] = x00c[3];
+    VertexData[4*3*n + 1*3 + 0] = x01c[1];
+    VertexData[4*3*n + 1*3 + 1] = x01c[2];
+    VertexData[4*3*n + 1*3 + 2] = x01c[3];
+    VertexData[4*3*n + 2*3 + 0] = x11c[1];
+    VertexData[4*3*n + 2*3 + 1] = x11c[2];
+    VertexData[4*3*n + 2*3 + 2] = x11c[3];
+    VertexData[4*3*n + 3*3 + 0] = x10c[1];
+    VertexData[4*3*n + 3*3 + 1] = x10c[2];
+    VertexData[4*3*n + 3*3 + 2] = x10c[3];
+    y = m2aux_get(&V->aux, obj_keys[DataMember]);
+    if (LogScale) {
+      y = log10(fabs(y));
+    }
+    y -= DataRange[0];
+    y /= DataRange[1] - DataRange[0];
+    color_map(y, &ColorData[3*n], ColorMap);
+  }
+}
+void DatasetController::refresh_cb(int user_id)
+{
+  instances[user_id]->refresh();
+}
+
+
+
+
 
 
 void color_map(double val, GLfloat rgb[3], int ct)
