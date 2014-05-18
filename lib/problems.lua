jzrake/m2

commit 60788f9f35d53857dce2d4c47a0bcfa4ef40bde0

Fix minor bugs in restart logic

lib/problems.lua
@@ -61,6 +61,7 @@ function TestProblem:run(user_config_callback, restart_file)
    -- the restart file if it exists, and finally the problem's
    -- set_runtime_defaults method.
    -----------------------------------------------------------------------------
+
    local runtime_cfg = {
       tmax = 0.4,
       output_path = 'data',
@@ -72,7 +73,8 @@ function TestProblem:run(user_config_callback, restart_file)
       local err, restored_runtime_cfg = serpent.load(h5f['runtime_cfg']:value())
       for k,v in pairs(restored_runtime_cfg) do
          runtime_cfg[k] = v
-      end
+      end -- required so that model_parameters are updated before m2 built
+      self:read_hdf5_problem_data(h5f)
       h5f:close()
    end
    if user_config_callback then

lib/m2app.lua
@@ -270,7 +270,7 @@ function m2Application:write_checkpoint_hdf5(fname, extras)
       local h5config = hdf5.Group(h5file, 'config')
 
       for i,member in ipairs(struct.members(self._m2)) do
-	 h5config[member] = tostring(self._m2[member])
+         h5config[member] = tostring(self._m2[member])
       end
       for i,member in ipairs(struct.members(self.status)) do
 	 if type(self.status[member]) == 'number' then
@@ -280,7 +280,7 @@ function m2Application:write_checkpoint_hdf5(fname, extras)
 	 end
       end
       if self._problem then
-	 self._problem:write_hdf5_problem_data(h5file)
+         self._problem:write_hdf5_problem_data(h5file)
       end
       for k,v in pairs(extras or { }) do
 	 h5file[k] = serpent.block(v)
@@ -335,8 +335,9 @@ function m2Application:read_checkpoint_hdf5(fname)
       local h5mem = h5status[member]
       if h5mem then -- in case status struct has new members since checkpoint
 	 local value = h5mem:value()
-	 if type(value) == 'number' then
-	    self.status[member] = value
+    if type(self.status[member]) == 'number' or
+       type(self.status[member]) == 'string' then
+       self.status[member] = value
 	 end
       end
    end

commit e22b216d023db08bbb638974b2fe3037bfd9d4b7

Fix a bug in restart logic introduced back in 60788f9f35

lib/problems.lua
@@ -19,10 +19,18 @@ function TestProblem:write_hdf5_problem_data(h5file)
    end
 end
 function TestProblem:read_hdf5_problem_data(h5file)
-   local h5mp = hdf5.Group(h5file, 'model_parameters', 'r+')
+   local h5mp = hdf5.Group(h5file, 'model_parameters', 'r')
    for k,v in pairs(self.model_parameters or { }) do
-      self.model_parameters[k] = h5mp[k]:value()
+      local str_val = h5mp[k]:value()
+      if type(v) == 'number' then
+         self.model_parameters[k] = tonumber(str_val)
+      elseif type(v) == 'boolean' then
+         self.model_parameters[k] = load("return "..str_val)()
+      elseif type(v) == 'string' then
+         self.model_parameters[k] = str_val
+      end
    end
+   h5mp:close()
 end
 function TestProblem:set_runtime_defaults(cfg) end
 function TestProblem:update_model_parameters(user_mp)