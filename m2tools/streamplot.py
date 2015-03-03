c93211b94cbcc30ed2fc74996671e9cc0cee6ce5
Jonathan Zrake <jonathan.zrake@gmail.com>
2015-03-03 15:14:28 -0800

    Add support for LIC

--- m2tools/streamplot.py
+++ m2tools/streamplot.py
@@ -3,6 +3,71 @@ import checkpoint
 
 
 
+
+
+
+class LineIntegralConvolutionPlot(command.Command):
+    _alias="lic"
+
+    def configure_parser(self, parser):
+        parser.add_argument('filenames', nargs='+')
+        parser.add_argument('--hardcopy', action='store_true')
+        parser.add_argument('-o', '--output', default=None)
+
+    def run(self, args):
+        import matplotlib.pyplot as plt
+
+        self.frame_number = 0
+        for filename in args.filenames:
+            self.run_file(args, filename)
+
+        if not args.hardcopy:
+            plt.show()
+
+    def run_file(self, args, filename):
+        import matplotlib.pyplot as plt
+        import numpy as np
+        import os
+        import lic.lic_internal
+
+        chkpt = checkpoint.Checkpoint(filename)
+        B1 = chkpt.cell_primitive['B1'][...].T
+        B2 = chkpt.cell_primitive['B2'][...].T
+        B3 = chkpt.cell_primitive['B3'][...].T
+        x, y = chkpt.cell_center_meshgrid
+        X = (1*x + 0*y).T
+        Y = (0*x + 1*y).T
+        B = (B1**2 + B2**2 + B3**2)**0.5
+
+        Nx, Ny = B.shape
+        kernel  = np.linspace(0, 1, 111).astype(np.float32)
+        texture = np.random.rand(Nx, Ny).astype(np.float32)
+        vectors = np.zeros([Nx, Ny, 2], dtype=np.float32)
+
+        vectors[:,:,0] = B1
+        vectors[:,:,1] = B2
+        image = lic.lic_internal.line_integral_convolution(vectors, texture, kernel)
+
+        if not args.hardcopy:
+            plt.figure()
+
+        plt.imshow(image, cmap='bone')
+
+        if args.hardcopy:
+            self.frame_number += 1
+            if args.output:
+                imgname = args.output
+            else:
+                imgname = "%s/frame-%04d.jpg" % (os.path.dirname(filename), self.frame_number)
+
+            print "writing", imgname
+            plt.savefig(imgname)
+            plt.clf()
+
+        chkpt.close()
+
+
+
 class StreamlinePlot(command.Command):
     _alias = "streamplot"
 
